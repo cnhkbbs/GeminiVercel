7c98fc98052f23d0220e528af8f3adad1655c285
cnhkbbs <cnhkemail@foxmail.com>
2023-12-15 16:27:18 +0800

    123

--- run.py
+++ run.py
@@ -1,25 +1,53 @@
-import requests
-import json
+# import requests
+# import json
+#
+# url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyBBWi-RvNvNqX3w3K5DnUfFR3ZGmMu69M0"
+# headers = {"Content-Type": "application/json"}
+#
+# data = {
+#     "prompt": {
+#         "text": "What's the meaning of life"
+#     }
+# }
+#
+# # 将数据转换为JSON格式
+# json_data = json.dumps(data)
+#
+# # 发送POST请求
+# response = requests.post(url, headers=headers, data=json_data)
+#
+# # 检查响应
+# if response.status_code == 200:
+#     # 打印生成的文本
+#     print(response.json())
+# else:
+#     print(f"请求失败，状态码: {response.status_code}")
+#     print(response.text)
 
-url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyBBWi-RvNvNqX3w3K5DnUfFR3ZGmMu69M0"
-headers = {"Content-Type": "application/json"}
+import pathlib
+import textwrap
 
-data = {
-    "prompt": {
-        "text": "Write a story about a magic backpack"
-    }
-}
+import google.generativeai as genai
 
-# 将数据转换为JSON格式
-json_data = json.dumps(data)
+from IPython.display import display
+from IPython.display import Markdown
 
-# 发送POST请求
-response = requests.post(url, headers=headers, data=json_data)
 
-# 检查响应
-if response.status_code == 200:
-    # 打印生成的文本
-    print(response.json())
-else:
-    print(f"请求失败，状态码: {response.status_code}")
-    print(response.text)
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+# api key
+genai.configure(api_key='AIzaSyBBWi-RvNvNqX3w3K5DnUfFR3ZGmMu69M0')
+
+# 列出模型
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
+
+model = genai.GenerativeModel('gemini-pro')
+
+response = model.generate_content("What is the meaning of life?")
+
+print(response.text)
