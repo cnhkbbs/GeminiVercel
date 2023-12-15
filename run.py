dca62f1cd2912977e5929a531a5687d5cdccf410
bui~bui~bui~bui <53457442+cnhkbbs@users.noreply.github.com>
2023-12-15 15:35:43 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+url = "https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=AIzaSyBBWi-RvNvNqX3w3K5DnUfFR3ZGmMu69M0"
+headers = {"Content-Type": "application/json"}
+
+data = {
+    "prompt": {
+        "text": "Write a story about a magic backpack"
+    }
+}
+
+# 将数据转换为JSON格式
+json_data = json.dumps(data)
+
+# 发送POST请求
+response = requests.post(url, headers=headers, data=json_data)
+
+# 检查响应
+if response.status_code == 200:
+    # 打印生成的文本
+    print(response.json())
+else:
+    print(f"请求失败，状态码: {response.status_code}")
+    print(response.text)
